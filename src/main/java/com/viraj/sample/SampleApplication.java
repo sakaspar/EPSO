e819aba077e7450de29d651646d6c9276d29c77b
Elyes20 <mohamedelyes.chamekh@esprit.tn>
2021-01-06 17:35:51 +0100

    k

--- src/main/java/com/viraj/sample/SampleApplication.java
+++ src/main/java/com/viraj/sample/SampleApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,6 +16,10 @@ public class SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
+	}
+	@GetMapping("/welcome/{name}")
+	public String wish(@PathVariable String name) {
+		return "Hi "+name+" Welcome to Doctor Service";
 	}
 
 }
